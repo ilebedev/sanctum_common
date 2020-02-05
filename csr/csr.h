db609844619cea17cf0bc318ed1f9b61bdb13afe
Jules Drean <drean@mit.edu>
2020-02-05 14:21:04 -0500

    Adapt macros so arguments can be expended

--- csr/csr.h
+++ csr/csr.h
@@ -39,22 +39,28 @@
 #define CSR_MEPARBASE 0x7c7
 #define CSR_MEPARMASK 0x7c8
 
-#define read_csr(reg) ({ unsigned long __tmp; \
+// Use double-macros so stringified argument can be expended by the pre-processor
+#define read_csr(reg) _read_csr(reg)
+#define _read_csr(reg) ({ unsigned long __tmp; \
   asm volatile ("csrr %0, " #reg : "=r"(__tmp)); \
   __tmp; })
 
-#define write_csr(reg, val) ({ \
+#define write_csr(reg, val) _write_csr(reg, val)
+#define _write_csr(reg, val) ({ \
   asm volatile ("csrw " #reg ", %0" :: "rK"(val)); })
 
-#define swap_csr(reg, val) ({ unsigned long __tmp; \
+#define swap_csr(reg, val) _swap_csr(reg, val)
+#define _swap_csr(reg, val) ({ unsigned long __tmp; \
   asm volatile ("csrrw %0, " #reg ", %1" : "=r"(__tmp) : "rK"(val)); \
   __tmp; })
 
-#define set_csr(reg, bit) ({ unsigned long __tmp; \
+#define set_csr(reg, bit) _set_csr(reg, bit)
+#define _set_csr(reg, bit) ({ unsigned long __tmp; \
   asm volatile ("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "rK"(bit)); \
   __tmp; })
 
-#define clear_csr(reg, bit) ({ unsigned long __tmp; \
+#define clear_csr(reg, bit) _clear_csr(reg, bit)
+#define _clear_csr(reg, bit) ({ unsigned long __tmp; \
   asm volatile ("csrrc %0, " #reg ", %1" : "=r"(__tmp) : "rK"(bit)); \
   __tmp; })
 
